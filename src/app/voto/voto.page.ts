3e97fe3007128799d9cb3ca6306255c8acb8f839
Felipe Chagas Bonadio <felipe.bonadio@etec.sp.gov.br>
2021-10-05 21:16:37 -0300

    voto

--- src/app/voto/voto.page.ts
+++ src/app/voto/voto.page.ts
@@ -11,4 +11,28 @@ export class VotoPage {
   nascimento de uma pessoa. O aplicativo deverá exibir uma mensagem que diga se ela poderá 
   ou não votar este ano (não é necessário considerar o mês em que a pessoa nasceu). Pesquise a regra de idade por votação.*/
 
+
+  public birthdate: number;
+  public age: number;
+  public msg: String;
+  public year: number;
+
+  public verify(): void{
+    this.calculateAge();
+    this.showMsg(this.age);
+  }
+  
+  public calculateAge(): void {
+    this.age = this.year - this.birthdate;    
+  }
+  public showMsg(age:number):void{
+    if (age<16){
+      this.msg= "Você não poderá votar este ano!"
+    } else if( age <18 || age>70){
+      this.msg = "Seu voto é opcional este ano!"
+    } else{
+      this.msg= "Seu voto é obrigatório este ano!"
+    }    
+  }
 }
+
